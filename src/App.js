492f47ec8af6f30e2dd300094a6e4ee6750d3fe9
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-06-28 20:57:34 +0200

    refactoring, import 3 components in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { firebase, db, googleAuth, turnCollectionToArray } from './firebase';
+import { firebase, fbDB } from './firebase';
 
+import AddEvent from './components/AddEvent/AddEvent'
+import AddEventsFromCSV from './components/AddEventsFromCSV/AddEventsFromCSV';
+import Events from './components/DisplayEvents/DisplayEvents';
 import styles from './App.module.css';
 
 class App extends Component {
@@ -9,83 +12,45 @@ class App extends Component {
   state = {
     values: {},
     filters_data: [],
-    filter_inputs: '',
+    column_names: '',
     all_events: [],
-    status: false
+    loginStatus: false
   };
-
-  signIn = () => {
-    firebase.auth().signInWithPopup(googleAuth);
-  }
-  signOut = () => {
-    firebase.auth().signOut();
-  }
 
   handleChange = (e) => {
     let vals = {...this.state.values};
     vals[e.target.name] = e.target.value;
     this.setState({ values: vals });
   }
-
-  handleSubmit = (e) => {
-    console.log(this.state.values);
-    e.preventDefault();
-    db.ref('db/events').push( {...this.state.values} );
-    this.reloadEvents();
-  }
   
-  submitFromCSV = (e) => {
-    e.preventDefault();
-    console.log(e, this.state.values)
-  }
+  
+
   reloadEvents = () => {
-    db.ref(`db/events`).once('value')
+    fbDB.ref(`db/events`).once('value')
     .then((snapshot) => {
       let dane = snapshot.val()
-      console.log(dane)
-      let items = [];
-      items = Object.keys(dane).map(item => {
-        console.log(dane[item].event_name)
-        return dane[item]
-      })
       this.setState({
-        all_events: items
+        all_events: Object.keys(dane).map(item => dane[item])
       })
+    })
+  }
+
+  createInputsList = () => {
+    fbDB.ref(`db/filter_names`).once('value')
+    .then((snapshot) => {
+      this.setState({ column_names: snapshot.val() });
     })
   }
 
   componentDidMount() {
-    db.ref(`db/filter_names`).once('value')
-    .then((snapshot) => {
-      let data = snapshot.val()
-      let fi = Object.keys(data).map(key => {
-        console.log(data[key].type)
-        let req = '';
-        return (
-            <div>
-              <label key={key}>
-                {data[key].full_name}:
-                <input type={data[key].type}
-                  name={key}
-                  onChange={this.handleChange}
-                  required={req}/>
-              </label>
-            </div>
-        )
-      })
-      this.setState({ 
-        filter_inputs: fi });
-    })
-
+    this.createInputsList();
     this.reloadEvents();
-
-    turnCollectionToArray();
 
     firebase.auth().onAuthStateChanged(user=>{
       if (user) {
-        this.setState({ status: true })
+        this.setState({ loginStatus: true })
       } else {
-        this.setState({ status: false })
+        this.setState({ loginStatus: false })
         console.log('!! NOT LOGGED IN !!')
       }
     })
@@ -93,46 +58,44 @@ class App extends Component {
 
   render() {
 
-    let nazwy = this.state.all_events.map((item,i)=><p key={i}>{item.event_name} - {item.city}</p>)
-    
     return (
 
       <div className={styles.App}>
+
+        <div className={styles.navbarTop}>
+          <div className="container">
+            <div className="row">
+              MENU
+            </div>
+          </div>
+        </div>
+        
         <div className="container">
           <div className="row">
-            <div className="col-md-3">
 
-              <h4>Dodaj wydarzenie</h4>
-              { this.state.status ?
-                <button onClick={this.signOut}>Logout</button>
-                :
-                <button onClick={this.signIn}>Login</button>
-              }
-              
-              <form onSubmit={this.handleSubmit} className={styles.form}>
-                  {this.state.filter_inputs}
-                  <input type="submit" value="Wyślij" />
-              </form>
-              
-              <form onSubmit={this.submitFromCSV} className={styles.form}>
-                  {this.state.filter_inputs ?
-                    <label>
-                      Importuj z formatu CSV:
-                      <textarea name="textArea" className={styles.textArea} value={this.state.value} onChange={this.handleChange} />
-                    </label> : ''
-                  }
-                  <input type="submit" value="Wyślij w formacie CSV" />
-              </form>
-
+            <div className="col-md-12">
+              <Events 
+                column_names={this.state.column_names}
+                all_events={this.state.all_events}/>
             </div>
-            <div className="col-md-9" style={{border: "1px solid blue"}}>
-              Select
-              <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </select>
-              {nazwy}
+
+            <div className="col-md-3">
+              <AddEvent 
+                loginStatus={this.state.loginStatus}
+                column_names={this.state.column_names}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                fbDB={fbDB}
+              />
+            </div>
+
+            <div className="col-md-3" >
+              <AddEventsFromCSV
+                loginStatus={this.state.loginStatus}
+                handleChange={this.handleChange}
+                value={this.state.values.textArea}
+                fbDB={fbDB}
+              />
             </div>
           </div>
         </div>
@@ -146,6 +109,3 @@ App.propTypes = {
 }
 
 export default App;
-
-
-// {styles.row} {styles.colLg3}   {styles.colLg9}
